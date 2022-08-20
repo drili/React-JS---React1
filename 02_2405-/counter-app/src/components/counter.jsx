25440ae65dd3483dc89d1df084593c6d58630398
drili <DriliB@hotmail.com>
2022-08-20 21:24:20 +0200

    :hammer: Rendering Classes Dynamically || Rendering Lists

--- 02_2405-/counter-app/src/components/counter.jsx
+++ 02_2405-/counter-app/src/components/counter.jsx
@@ -3,23 +3,34 @@ import React, { Component } from 'react'
 class Counter extends Component {
     state = {
         count: 0,
-        imageUrl: "https://picsum.photos/200"
+        imageUrl: "https://picsum.photos/200",
+        tags: ["tag1", "tag2", "tag3"]
     }
 
     styles = {
-        fontSize: 10,
+        fontSize: 14,
         fontWeight: "bold"
     }
 
-    render() { 
+    render() {
         return (
             <>
                 {/* <img src={this.state.imageUrl} alt="" /> */}
 
-                <span style={ this.styles } className='badge bg-primary m-2'>{this.formatCount()}</span>
+                <span style={ this.styles } className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className='btn btn-secondary btn-sm'>Increment</button>
+
+                <ul>
+                    { this.state.tags.map(tag => <li key={tag}>{ tag }</li>) }
+                </ul>
             </>
         )
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 "
+        classes += (this.state.count === 0) ? "bg-warning" : "bg-primary"
+        return classes
     }
 
     formatCount() {
