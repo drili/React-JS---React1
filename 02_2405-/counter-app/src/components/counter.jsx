097d824e1cac284fd175b87c4509ff713efefa23
drili <DriliB@hotmail.com>
2022-08-20 20:57:45 +0200

    :hammer: INIT || Specifying Children

---
+++ 02_2405-/counter-app/src/components/counter.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Counter extends Component {
+    render() { 
+        return (
+            <>
+                <h1>Hello World</h1>
+                <button>Increment</button>
+            </>
+        )
+    }
+}
+ 
+export default Counter;
